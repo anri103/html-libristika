2ba7dc2bc9fa19b872f85c5f9db62e7c4dc63197
anri103 <ianrianri@gmail.com>
2025-03-21 00:14:26 +0300

    add sign-pages, add forgot-password page

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const { src, dest, series, parallel, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
 const del = require('del');
-const fileInclude = require('gulp-file-include'); // Для импорта HTML-компонентов
+const fileInclude = require('gulp-file-include');
 
 // Пути к исходным файлам и папке назначения
 const paths = {
@@ -24,7 +24,7 @@ const paths = {
   },
   pages: {
     src: 'src/pages/**/*.html',
-    dest: 'dist/pages/'
+    dest: 'dist/'
   },
   components: {
     src: 'src/components/**/*.html',
@@ -34,7 +34,31 @@ const paths = {
 
 // Очистка папки dist
 function clean() {
-  return del(['dist']);
+  return del(['dist/**/*']);
+}
+
+// Удаление файлов из dist, если они удалены или переименованы в src
+function sync() {
+  return watch('src/**/*.html', { events: ['unlink', 'rename'] }, function (event, file) {
+    if (event === 'unlink') {
+      // Удаление файла
+      const filePath = file.path.replace('src', 'dist');
+      return del(filePath);
+    } else if (event === 'rename') {
+      // Переименование файла
+      const oldFilePath = file.oldPath.replace('src', 'dist');
+      const newFilePath = file.path.replace('src', 'dist');
+      return del(oldFilePath).then(() => {
+        return src(newFilePath.replace('dist', 'src'))
+          .pipe(fileInclude({
+            prefix: '@@',
+            basepath: '@file'
+          }))
+          .pipe(dest(paths.pages.dest))
+          .pipe(browserSync.stream());
+      });
+    }
+  });
 }
 
 // Компиляция SCSS в CSS
@@ -62,14 +86,14 @@ function html() {
 
 // Копирование шрифтов
 function fonts() {
-  return src(paths.fonts.src, { encoding: false })
+  return src(paths.fonts.src, { encoding: false, allowEmpty: true })
     .pipe(dest(paths.fonts.dest))
     .pipe(browserSync.stream());
 }
 
 // Копирование изображений
 function images() {
-  return src(paths.images.src, { encoding: false })
+  return src(paths.images.src, { encoding: false, allowEmpty: true })
     .pipe(dest(paths.images.dest))
     .pipe(browserSync.stream());
 }
@@ -85,12 +109,16 @@ function scripts() {
     .pipe(browserSync.stream());
 }
 
+// Удаление пустых папок
+function cleanEmptyDirs() {
+  return del(['dist/**/*', '!dist/**/*.*']); // Удаляем только пустые папки
+}
+
 // Запуск сервера и отслеживание изменений
 function serve() {
   browserSync.init({
     server: {
-      baseDir: './dist',
-      index: 'pages/index.html'
+      baseDir: './dist'
     }
   });
 
@@ -103,6 +131,7 @@ function serve() {
 
 // Основные задачи
 exports.clean = clean;
+exports.sync = sync;
 exports.styles = styles;
 exports.scripts = scripts;
 exports.images = images;
@@ -111,7 +140,7 @@ exports.html = html;
 exports.serve = serve;
 
 // Сборка проекта
-exports.build = series(clean, parallel(styles, scripts, images, fonts, html));
+exports.build = series(clean, parallel(styles, scripts, images, fonts, html), cleanEmptyDirs);
 
 // Задача по умолчанию
-exports.default = series(clean, parallel(styles, scripts, images, fonts, html), serve);
+exports.default = series(clean, parallel(styles, scripts, images, fonts, html), parallel(sync, serve));
