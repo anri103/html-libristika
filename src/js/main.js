e52b601568300453d606794d34958fa2cce9319c
anri103 <ianrianri@gmail.com>
2025-06-06 01:48:15 +0300

    -

--- src/js/main.js
+++ src/js/main.js
@@ -1,53 +1,44 @@
-// Sliders
+window.onload = () => {
+    // hero slider
+    const swiperHero = document.querySelector('.swiperHero');
+    if (swiperHero) {
+        var swiper = new Swiper(swiperHero, {
+            loop: true,
+            autoplay: {
+                delay: 5000,
+                disableOnInteraction: false, // Continue autoplay after interaction
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+        });
+    }
+    // slider with cards
+    const swiperCards = document.querySelectorAll('.swiperCards');
+    swiperCards.forEach(slider => {
+        new Swiper(slider, {
+            slidesPerView: 'auto',
+            spaceBetween: 20,
+            freeMode: true,
+            loop: false,
+            navigation: {
+                nextEl: slider.querySelector('.swiper-button-next'),
+                prevEl: slider.querySelector('.swiper-button-prev'),
+            },
+            breakpoints: {
+                992: {
+                    spaceBetween: 24,
+                },
+            },
+        });
+    });
 
-const swiperHero = new Swiper('.swiperHero', {
-    loop: true,
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false, // Continue autoplay after interaction
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-});
-
-const swiperCards = new Swiper('.swiperCards', {
-    slidesPerView: 'auto',
-    spaceBetween: 20,
-    // centeredSlides: true,
-    freeMode: true,
-    loop: false,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        992: {
-            spaceBetween: 24,
-        },
-    },
-});
-
-// Button Add to whishlist
-
-document.addEventListener('DOMContentLoaded', function () {
-    const wishlistButtons = document.querySelectorAll('.btn-add-whishlist');
-
-    wishlistButtons.forEach(button => {
-        button.addEventListener('click', function () {
+    // button add to whishlist
+    const wishlistBtn = document.querySelectorAll('.btn-add-whishlist');
+    wishlistBtn.forEach(button => {
+        button.addEventListener('click', () => {
             button.classList.toggle('js-active');
         });
     });
-});
-
-// Validate Forms checkbox
-// Функция валидации только для демонстрации, НЕ ВКЛЮЧАТЬ В ИТОГОВЫЙ ПРОЕКТ!
-
-function validateForm(checkbox) {
-    const form = checkbox.closest('form');
-    const checkboxes = form.querySelectorAll('input[type="checkbox"][onchange]');
-    const submitButton = form.querySelector('button[type="submit"]');
-    const allChecked = Array.from(checkboxes).every(cb => cb.checked);
-    submitButton.disabled = !allChecked;
-  }
+}
