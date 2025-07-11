3918988386a681cd783c218cc9d70b15b3f8e552
anri103 <ianrianri@gmail.com>
2025-07-12 00:09:16 +0300

    -

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,14 @@ const browserSync = require('browser-sync').create();
 const del = require('del');
 const fileInclude = require('gulp-file-include');
 const concat = require('gulp-concat');
+const merge = require('merge-stream');
 
 // Пути
 const paths = {
   styles: {
     src: 'src/scss/**/*.scss',
     dest: 'dist/css/',
+    main: 'src/scss/main.scss',
     libs: [
       'node_modules/@coreui/coreui-pro/dist/css/coreui.min.css',
       'node_modules/swiper/swiper-bundle.min.css'
@@ -48,22 +50,32 @@ function clean() {
 
 // Конкатенация + SCSS -> CSS
 function styles() {
-  return src(paths.styles.src)
+  // Получаем библиотеки CSS
+  const libs = src(paths.styles.libs);
+  
+  // Компилируем SCSS
+  const main = src(paths.styles.main)
     .pipe(sass().on('error', sass.logError))
+  // Объединяем библиотеки и скомпилированный SCSS в один файл
+  return merge(libs, main)
+    .pipe(concat('main.css'))
     .pipe(dest(paths.styles.dest))
     .pipe(browserSync.stream());
 }
 
 // JS
-function scripts() {
-  return src([
-    'node_modules/@coreui/coreui-pro/dist/js/coreui.bundle.min.js',
-    'node_modules/swiper/swiper-bundle.min.js',
-    paths.scripts.src
-  ])
+function scriptsLibs() {
+  return src(paths.scripts.libs)
+    .pipe(dest(paths.scripts.dest));
+}
+
+function scriptsProject() {
+  return src(paths.scripts.src)
     .pipe(dest(paths.scripts.dest))
     .pipe(browserSync.stream());
 }
+
+const scripts = parallel(scriptsLibs, scriptsProject);
 
 // HTML
 function html() {
@@ -99,7 +111,7 @@ function serve() {
   });
 
   watch(paths.styles.src, styles);
-  watch(paths.scripts.src, scripts);
+  watch(paths.scripts.src, scriptsProject);
   watch(paths.images.src, images);
   watch(paths.fonts.src, fonts);
   watch([paths.pages.src, paths.components.src], html);
