70182b552b431cb5c14b7fc0c84f574a0b769869
anri103 <ianrianri@gmail.com>
2025-07-29 19:55:19 +0300

    -

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,36 @@ const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
 const del = require('del');
 const fileInclude = require('gulp-file-include');
-const merge = require('merge-stream');
 
-// Пути к исходным файлам и папке назначения
+// Пути
 const paths = {
+  pages: {
+    src: 'src/pages/**/*.html',
+    dest: 'dist/'
+  },
+  components: {
+    src: 'src/components/**/*.html'
+  },
   styles: {
     src: 'src/scss/**/*.scss',
     dest: 'dist/css/'
   },
   scripts: {
     src: 'src/js/**/*.js',
+    dest: 'dist/js/'
+  },
+  vendorCSS: {
+    src: [
+      'node_modules/@coreui/coreui-pro/dist/css/coreui.min.css',
+      'node_modules/swiper/swiper-bundle.min.css'
+    ],
+    dest: 'dist/css/'
+  },
+  vendorJS: {
+    src: [
+      'node_modules/@coreui/coreui-pro/dist/js/coreui.bundle.min.js',
+      'node_modules/swiper/swiper-bundle.min.js'
+    ],
     dest: 'dist/js/'
   },
   images: {
@@ -22,49 +42,15 @@ const paths = {
   fonts: {
     src: 'src/fonts/**/*',
     dest: 'dist/fonts/'
-  },
-  pages: {
-    src: 'src/pages/**/*.html',
-    dest: 'dist/'
-  },
-  components: {
-    src: 'src/components/**/*.html',
-    dest: 'dist/components/'
   }
 };
 
-// Очистка папки dist
+// Очистка dist
 function clean() {
-  return del(['dist/**/*']);
+  return del(['dist']);
 }
 
-// Компиляция SCSS в CSS и копирование CSS-файлов
-function styles() {
-  const scssStream = src(paths.styles.src)
-    .pipe(sass().on('error', sass.logError));
-
-  const cssStream = src([
-    'node_modules/@coreui/coreui-pro/dist/css/coreui.min.css',
-    'node_modules/swiper/swiper-bundle.min.css'
-  ]);
-
-  return merge(scssStream, cssStream)
-    .pipe(dest(paths.styles.dest))
-    .pipe(browserSync.stream());
-}
-
-// Копирование JS-файлов
-function scripts() {
-  return src([
-    'node_modules/@coreui/coreui-pro/dist/js/coreui.bundle.min.js',
-    'node_modules/swiper/swiper-bundle.min.js',
-    paths.scripts.src
-  ])
-    .pipe(dest(paths.scripts.dest))
-    .pipe(browserSync.stream());
-}
-
-// Копирование и обработка HTML-файлов с импортом компонентов
+// HTML (include компонентов)
 function html() {
   return src(paths.pages.src)
     .pipe(fileInclude({
@@ -75,10 +61,32 @@ function html() {
     .pipe(browserSync.stream());
 }
 
-// Копирование шрифтов
-function fonts() {
-  return src(paths.fonts.src, { encoding: false, allowEmpty: true })
-    .pipe(dest(paths.fonts.dest))
+// SCSS
+function styles() {
+  return src(paths.styles.src)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest(paths.styles.dest))
+    .pipe(browserSync.stream());
+}
+
+// JS
+function scripts() {
+  return src(paths.scripts.src)
+    .pipe(dest(paths.scripts.dest))
+    .pipe(browserSync.stream());
+}
+
+// Копирование vendor CSS
+function vendorCSS() {
+  return src(paths.vendorCSS.src)
+    .pipe(dest(paths.vendorCSS.dest))
+    .pipe(browserSync.stream());
+}
+
+// Копирование vendor JS
+function vendorJS() {
+  return src(paths.vendorJS.src)
+    .pipe(dest(paths.vendorJS.dest))
     .pipe(browserSync.stream());
 }
 
@@ -89,8 +97,15 @@ function images() {
     .pipe(browserSync.stream());
 }
 
-// Запуск сервера и отслеживание изменений
-function serve() {
+// Копирование шрифтов
+function fonts() {
+  return src(paths.fonts.src, { encoding: false, allowEmpty: true })
+    .pipe(dest(paths.fonts.dest))
+    .pipe(browserSync.stream());
+}
+
+// Запуск сервера и слежка
+function watchFiles() {
   browserSync.init({
     server: {
       baseDir: './dist'
@@ -105,9 +120,22 @@ function serve() {
 }
 
 // Основные задачи
+exports.clean = clean;
+exports.html = html;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.vendorCSS = vendorCSS;
+exports.vendorJS = vendorJS;
+exports.images = images;
+exports.fonts = fonts;
 
-// Сборка проекта
-exports.build = series(clean, parallel(styles, scripts, images, fonts, html));
+exports.default = series(
+  clean,
+  parallel(html, styles, scripts, vendorCSS, vendorJS, images, fonts),
+  watchFiles
+);
 
-// Задача по умолчанию
-exports.default = series(clean, parallel(styles, scripts, images, fonts, html), serve);
+exports.build = series(
+  clean,
+  parallel(html, styles, scripts, vendorCSS, vendorJS, images, fonts)
+);
